e78172fbf6665dee41232933f2e6f2e0a0ca371c
uRo3YA <ltg8693@gmail.com>
2022-10-07 10:08:39 +0900

    CRUD 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,12 @@ from . import views
 app_name = "reviews"
 urlpatterns = [
     path("", views.index, name="index"),
+    # 영화 데이터 정보 조회 detail
+    path("<int:pk>/", views.detail, name="detail"),
+    # - 영화 데이터 생성 create
+    path("create/", views.create, name="create"),
+    # - 영화 데이터 수정 update
+    path("<int:pk>/update/", views.update, name="update"),
+    # - 영화 데이터 삭제 delete
+    path("<int:pk>/delete/", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
 def index(request):
     # 게시글을 가져와서..
-
+    reviews = Review.objects.order_by("-pk")
     # Template에 전달한다.
+    context = {"reviews": reviews}
+    return render(request, "reviews/index.html", context)
 
-    return render(request, "reviews/index.html")
+
+def detail(request, pk):
+    # 특정 글을 가져온다.
+    review = Review.objects.get(pk=pk)
+    # template에 객체 전달
+    context = {"review": review}
+    return render(request, "reviews/detail.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:index")
+    else:
+        review_form = ReviewForm()
+    context = {"review_form": review_form}
+    return render(request, "reviews/create.html", context=context)
+
+
+def update(request, pk):
+    movie = Review.objects.get(pk=pk)
+    if request.method == "POST":
+        # POST : input 값 가져와서, 검증하고, DB에 저장
+        review_form = ReviewForm(request.POST, instance=movie)
+        if review_form.is_valid():
+            # 유효성 검사 통과하면 저장하고, 상세보기 페이지로
+            review_form.save()
+            return redirect("reviews:detail", movie.pk)
+        # 유효성 검사 통과하지 않으면 => context 부터해서 오류메시지 담긴 review_form 랜더링
+    else:
+        # GET : Form을 제공
+        review_form = ReviewForm(instance=movie)
+    context = {"review_form": review_form}
+    return render(request, "reviews/update.html", context)
+
+
+def delete(request, pk):
+
+    review = Review.objects.get(pk=pk)
+    review.delete()
+
+    return redirect("reviews:index")
